6dbec68cf5db957937e8654f9795a8f1c19e6455
shotaro yamamoto <shotoron@gmail.com>
2020-08-29 15:50:01 +0900

    [config]タイムゾーンを東京に設定

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,7 @@ Bundler.require(*Rails.groups)
 
 module DevExam
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
